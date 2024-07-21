309bea7dd777c49da257b1a6989488936a8ed879
Umut Özdemir <umutozdemir3555@gmail.com>
2024-07-21 19:18:38 +0300

    İlaç ekleme sayfası ayrıldı. Veteriner Hekim girişi yenilendi.
    Buradaki gibi hazırlanmış sayfaların fonksiyonlarını ayrı python dosyalarına çekebiliriz.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import mysql.connector
 import pandas as pd
 import re
 from utils import get_data, update_data, get_highest_id, insert_data
-
+from ilacEkleme import add_medicine_page
 
 # st.session_state kullanarak kullanıcı_id tanımlama
 if 'kullanıcı_id' not in st.session_state:
@@ -15,6 +15,8 @@ if 'rol' not in st.session_state:
 if 'admin_id' not in st.session_state:
     st.session_state.admin_id = None
 
+if 'veteriner_id' not in st.session_state:
+    st.session_state.veteriner_id = None
 
 
 # Ana sayfa fonksiyonu
@@ -29,6 +31,7 @@ def main_page():
         """,
         unsafe_allow_html=True
     )
+
     mail = st.text_input("Mail Adresi", key="login_email")
     sifre = st.text_input("Şifre", type="password", key="login_password")
     
@@ -68,9 +71,20 @@ def main_page():
     
     with col3:
         if st.button("Veteriner Hekim Giriş"):
-            st.session_state.prev_page = st.session_state.page
-            st.session_state.page = "Veterinarian Main"
-            st.experimental_rerun()
+            query =  "SELECT * FROM bil372_project.kullanıcı where email = '{}' and şifre = '{}' and rol ='{}'".format(mail, sifre,"veteriner")
+            data = get_data(query)
+            
+            if data is not None and not data.empty:
+                st.session_state.veteriner_id = data.iloc[0]['KullanıcıID'] 
+                st.session_state.rol = 'Veteriner'
+
+                st.session_state.prev_page = st.session_state.page
+                st.session_state.page = "Veterinarian Main"
+                st.experimental_rerun()
+            else:
+                st.write("Veteriner bulunamadı veya yanlış giriş bilgileri.")
+
+            
 
     if st.button("Kayıt Ol"):
         st.session_state.prev_page = st.session_state.page
@@ -232,52 +246,7 @@ def add_veterinarian_page():
     if st.button("Ekle"):
         st.write("Veteriner hekim eklendi!")  # Placeholder for adding veterinarian to the database
 
-# İlaç ekle sayfası fonksiyonu
-def add_medicine_page():
-    st.title("İlaç Ekle")
-    if st.button("Geri"):
-        st.session_state.page = st.session_state.prev_page
-        st.experimental_rerun()
-    ilac_ad = st.text_input("İlaç Adı")
-    ilac_toplam_ucret = st.text_input("Toplam Ücret")
-    ilac_miktar = st.text_input("Miktar")
 
-    if st.button("Ekle"):
-        query =  "SELECT * FROM bil372_project.ilaçlar where isim = '{}' and adminId = '{}'".format(ilac_ad, str(st.session_state.admin_id))
-        data = get_data(query)
-
-        # Eger bu admin tarafından eklenen ilac zaten bulunuyorsa eski veriyi güncelliyoruz ekliyoruz
-        if data is not None and not data.empty:
-            # Veri mevcut, güncelle
-            ilac_id = data.iloc[0]['İlaçID']
-            # TODO kodu temizle
-            eski_toplam_ucret = data.iloc[0]['Fiyat']
-            eski_toplam_miktar = data.iloc[0]['Miktar']
-            
-            guncel_ucret = int(eski_toplam_ucret) + int(ilac_toplam_ucret)
-            guncel_miktar = int(eski_toplam_miktar) + int(ilac_miktar)
-
-            update_ilac = """
-            UPDATE ilaçlar
-            SET Fiyat = %s, Miktar = %s
-            WHERE ilaçId = %s
-            """
-            update_params = (str(guncel_ucret), str(guncel_miktar), str(ilac_id))
-            update_data(update_ilac, update_params)
-            
-        # Eger bu admin tarafından eklenen ilac zaten bulunmuyorsa yenisini ekliyoruz
-        else:
-            id = get_highest_id('ilaçlar', 'İlaçID') + 1
-            insert_ilac_admin = """
-            INSERT INTO ilaçlar (ilaçId, isim, Fiyat, Miktar, AdminId)
-            VALUES (%s, %s, %s, %s, %s)
-            """
-            # Burada str()'ler gereksiz olabilir
-            params = (str(id), ilac_ad, str(ilac_toplam_ucret), str(ilac_miktar), str(st.session_state.admin_id))
-
-            insert_data(insert_ilac_admin, params)
-        
-        st.write("İlaç eklendi!")  # Placeholder for adding medicine to the database
 
 # Admin bilgileri sayfası fonksiyonu
 def admin_info_page():
---
+++ ilacEkleme.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import mysql.connector
+import pandas as pd
+import re
+from utils import get_data, update_data, get_highest_id, insert_data
+
+
+# İlaç ekle sayfası fonksiyonu
+def add_medicine_page():
+    st.title("İlaç Ekle")
+    if st.button("Geri"):
+        st.session_state.page = st.session_state.prev_page
+        st.experimental_rerun()
+    ilac_ad = st.text_input("İlaç Adı")
+    ilac_toplam_ucret = st.text_input("Toplam Ücret")
+    ilac_miktar = st.text_input("Miktar")
+
+    if st.button("Ekle"):
+        query =  "SELECT * FROM bil372_project.ilaçlar where isim = '{}' and adminId = '{}'".format(ilac_ad, str(st.session_state.admin_id))
+        data = get_data(query)
+
+        # Eger bu admin tarafından eklenen ilac zaten bulunuyorsa eski veriyi güncelliyoruz ekliyoruz
+        if data is not None and not data.empty:
+            # Veri mevcut, güncelle
+            ilac_id = data.iloc[0]['İlaçID']
+            # TODO kodu temizle
+            eski_toplam_ucret = data.iloc[0]['Fiyat']
+            eski_toplam_miktar = data.iloc[0]['Miktar']
+            
+            guncel_ucret = int(eski_toplam_ucret) + int(ilac_toplam_ucret)
+            guncel_miktar = int(eski_toplam_miktar) + int(ilac_miktar)
+
+            update_ilac = """
+            UPDATE ilaçlar
+            SET Fiyat = %s, Miktar = %s
+            WHERE ilaçId = %s
+            """
+            update_params = (str(guncel_ucret), str(guncel_miktar), str(ilac_id))
+            update_data(update_ilac, update_params)
+
+        # Eger bu admin tarafından eklenen ilac zaten bulunmuyorsa yenisini ekliyoruz
+        else:
+            id = get_highest_id('ilaçlar', 'İlaçID') + 1
+            insert_ilac_admin = """
+            INSERT INTO ilaçlar (ilaçId, isim, Fiyat, Miktar, AdminId)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+            # Burada str()'ler gereksiz olabilir
+            params = (str(id), ilac_ad, str(ilac_toplam_ucret), str(ilac_miktar), str(st.session_state.admin_id))
+
+            insert_data(insert_ilac_admin, params)
+        
+        st.write("İlaç eklendi!")  # Placeholder for adding medicine to the database
